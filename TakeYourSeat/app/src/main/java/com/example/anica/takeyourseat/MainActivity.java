77d6ed14d71f1ec37886ca8b9a2539c44cd17c34
anica <anica.sutic@gmail.com>
2017-04-22 12:02:33 +0200

    Initial commit with login layout

---
+++ TakeYourSeat/app/src/main/java/com/example/anica/takeyourseat/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.anica.takeyourseat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.content.Intent;
+
+import org.w3c.dom.Text;
+
+import reviewer.activities.RegisterActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private TextView username;
+    private TextView pass;
+    private Button logIn;
+    private TextView registerHere;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        username = (TextView)findViewById(R.id.username);
+        pass = (TextView)findViewById(R.id.pass);
+        logIn = (Button)findViewById(R.id.signIn);
+        registerHere = (TextView)findViewById(R.id.register);
+
+        registerHere.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent registerIntent = new Intent(MainActivity.this, RegisterActivity.class);
+                MainActivity.this.startActivity(registerIntent);
+            }
+        });
+
+    }
+}
