f3009b5e37daaef503d04bed63f8fccd264fc1ed
Nenad <nenad.rad933@gmail.com>
2017-04-25 02:29:02 +0200

    Added mocked list of restaurants inside home activity.

--- TakeYourSeat/app/src/main/java/reviewer/activities/HomePageActivity.java
+++ TakeYourSeat/app/src/main/java/reviewer/activities/HomePageActivity.java
@@ -8,15 +8,36 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.Toast;
 
 import com.example.anica.takeyourseat.R;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class HomePageActivity extends AppCompatActivity {
 
     private String[] navDrawerItemTitles;
     private DrawerLayout drawerLayout;
     private ListView drawerList;
+
+    private String[] listviewTitle = new String[]{
+            "Restaurant 1", "Restaurant 2", "Restaurant 3", "Restaurant 4",
+            "Restaurant 5", "Restaurant 6", "Restaurant 7", "Restaurant 8",
+    };
+
+    private int[] listviewImage = new int[]{
+            R.drawable.restaurant_icon, R.drawable.restaurant_icon, R.drawable.restaurant_icon, R.drawable.restaurant_icon,
+            R.drawable.restaurant_icon, R.drawable.restaurant_icon, R.drawable.restaurant_icon, R.drawable.restaurant_icon,
+
+    };
+
+    private String[] listviewShortDescription = new String[]{
+            "Restaurant 1 Description", "Restaurant 2 Description", "Restaurant 3 Description", "Restaurant 4 Description",
+            "Restaurant 5 Description", "Restaurant 6 Description", "Restaurant 7 Description", "Restaurant 8 Description",
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,13 +54,31 @@ public class HomePageActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
-                if(position == 3) {
+                if (position == 3) {
                     Intent settings = new Intent(HomePageActivity.this, SettingsActivity.class);
-                    startActivityForResult(settings,0);
+                    startActivityForResult(settings, 0);
                 }
 
             }
 
         });
+
+        List<HashMap<String, String>> aList = new ArrayList<HashMap<String, String>>();
+
+        for (int i = 0; i < 8; i++) {
+            HashMap<String, String> hm = new HashMap<String, String>();
+            hm.put("listview_title", listviewTitle[i]);
+            hm.put("listview_description", listviewShortDescription[i]);
+            hm.put("listview_image", Integer.toString(listviewImage[i]));
+            aList.add(hm);
+        }
+
+        String[] from = {"listview_image", "listview_title", "listview_description"};
+        int[] to = {R.id.listview_image, R.id.listview_item_title, R.id.listview_item_short_description};
+
+        SimpleAdapter simpleAdapter = new SimpleAdapter(getBaseContext(), aList, R.layout.restaurant_list_item, from, to);
+        ListView restaurantListView = (ListView)findViewById(R.id.restaurant_list_view);
+        restaurantListView.setAdapter(simpleAdapter);
     }
 }
+
