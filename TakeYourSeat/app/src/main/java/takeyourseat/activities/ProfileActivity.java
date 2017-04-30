c0334865e8535bd0e04bb2f9d616f2b9d422e266
ivana57 <inna0507@gmail.com>
2017-04-30 22:36:55 +0200

    Added User bean and displayed user details.

--- TakeYourSeat/app/src/main/java/takeyourseat/activities/ProfileActivity.java
+++ TakeYourSeat/app/src/main/java/takeyourseat/activities/ProfileActivity.java
@@ -2,16 +2,31 @@ package takeyourseat.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.example.anica.takeyourseat.R;
 
+import takeyourseat.beans.User;
+
 public class ProfileActivity extends AppCompatActivity {
+
+    public User currentUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-    }
 
+        currentUser = new User();
+        currentUser.setFirstName("Ime");
+        currentUser.setLastName("Prezime");
+
+        TextView firstNameView = (TextView)findViewById(R.id.textViewFirstName);
+        TextView lastNameView = (TextView)findViewById(R.id.textViewLastName);
+
+        firstNameView.setText(currentUser.getFirstName());
+        lastNameView.setText(currentUser.getLastName());
+
+    }
 
 }
