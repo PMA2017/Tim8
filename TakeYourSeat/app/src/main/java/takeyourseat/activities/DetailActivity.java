b595d5c3170ae843825d9cd687cf81e37b5c941f
anica <anica.sutic@gmail.com>
2017-05-02 21:44:00 +0200

    Added tabbed activity with view pager and navigation.

--- TakeYourSeat/app/src/main/java/takeyourseat/activities/DetailActivity.java
+++ TakeYourSeat/app/src/main/java/takeyourseat/activities/DetailActivity.java
@@ -1,5 +1,7 @@
 package takeyourseat.activities;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -7,13 +9,32 @@ import android.widget.TextView;
 
 import com.example.anica.takeyourseat.R;
 
+import takeyourseat.adapters.ViewPagerAdapter;
+import takeyourseat.fragments.AboutFragment;
+import takeyourseat.fragments.LocationFragment;
+import takeyourseat.fragments.MenuFragment;
+import takeyourseat.fragments.TableFragment;
+
 public class DetailActivity extends AppCompatActivity {
 
     private TextView resName;
-    private Toolbar toolbar;
+    private TabLayout tabs;
+    private ViewPager viewPager;
+    private ViewPagerAdapter viewPagerAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_detail);
+
+        tabs = (TabLayout) findViewById(R.id.tabLayout);
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+        viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+        viewPagerAdapter.addFragments(new MenuFragment(), "Menu");
+        viewPagerAdapter.addFragments(new TableFragment(), "Tables");
+        viewPagerAdapter.addFragments(new LocationFragment(), "Location");
+        viewPagerAdapter.addFragments(new AboutFragment(), "About");
+        viewPager.setAdapter(viewPagerAdapter);
+        tabs.setupWithViewPager(viewPager);
+
     }
 }
