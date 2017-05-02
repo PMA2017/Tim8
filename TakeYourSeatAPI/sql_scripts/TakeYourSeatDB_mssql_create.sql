a459cb2885b1f38acb9ba231b3147fe3f9229ac1
Nenad <nenad.rad933@gmail.com>
2017-05-02 13:57:55 +0200

    Updated sql script for database.

--- TakeYourSeatAPI/sql_scripts/TakeYourSeatDB_mssql_create.sql
+++ TakeYourSeatAPI/sql_scripts/TakeYourSeatDB_mssql_create.sql
@@ -1,213 +1,175 @@
-CREATE TABLE [Restaurant] (
-	Id int NOT NULL,
-	Name varchar(50) NOT NULL,
-	Description varchar(300) NOT NULL,
-	Manager int NOT NULL,
-	Image image NOT NULL,
-	Phone varchar(50) NOT NULL,
-	Email varchar(100) NOT NULL,
-	WebSite varchar(50) NOT NULL,
-	Address int NOT NULL,
-  CONSTRAINT [PK_RESTAURANT] PRIMARY KEY CLUSTERED
-  (
-  [Id] ASC
-  ) WITH (IGNORE_DUP_KEY = OFF)
+-- Created by Vertabelo (http://vertabelo.com)
+-- Last modification date: 2017-05-02 11:49:08.641
 
-)
-GO
-CREATE TABLE [User] (
-	Id int NOT NULL,
-	Name varchar(50) NOT NULL,
-	Address varchar(100) NOT NULL,
-	Email varchar(100) NOT NULL,
-	Password varchar(100) NOT NULL,
-	Role int NOT NULL,
-  CONSTRAINT [PK_User] PRIMARY KEY CLUSTERED
-  (
-  [Id] ASC
-  ) WITH (IGNORE_DUP_KEY = OFF)
+-- tables
+-- Table: Address
+CREATE TABLE Address (
+    Id int  NOT NULL IDENTITY,
+    Coutry varchar(50)  NOT NULL,
+    Street varchar(100)  NOT NULL,
+    Number varchar(10)  NOT NULL,
+    Location int  NOT NULL,
+    CONSTRAINT Address_pk PRIMARY KEY  (Id)
+);
 
-)
-GO
-CREATE TABLE [Role] (
-	Id int NOT NULL,
-	Name varchar(20) NOT NULL,
-  CONSTRAINT [PK_ROLE] PRIMARY KEY CLUSTERED
-  (
-  [Id] ASC
-  ) WITH (IGNORE_DUP_KEY = OFF)
+-- Table: Comment
+CREATE TABLE Comment (
+    Id int  NOT NULL IDENTITY,
+    Description varchar(300)  NOT NULL,
+    "User" int  NOT NULL,
+    Restaurant int  NOT NULL,
+    CONSTRAINT Comment_pk PRIMARY KEY  (Id)
+);
 
-)
-GO
-CREATE TABLE [Location] (
-	Id int NOT NULL,
-	Latitude decimal NOT NULL,
-	Longitude decimal NOT NULL,
-  CONSTRAINT [PK_LOCATION] PRIMARY KEY CLUSTERED
-  (
-  [Id] ASC
-  ) WITH (IGNORE_DUP_KEY = OFF)
+-- Table: Location
+CREATE TABLE Location (
+    Id int  NOT NULL IDENTITY,
+    Latitude decimal  NOT NULL,
+    Longitude decimal  NOT NULL,
+    CONSTRAINT Location_pk PRIMARY KEY  (Id)
+);
 
-)
-GO
-CREATE TABLE [Address] (
-	Id int NOT NULL,
-	Country varchar(50) NOT NULL,
-	Street varchar(100) NOT NULL,
-	Number varchar(10) NOT NULL,
-	Location int NOT NULL,
-  CONSTRAINT [PK_ADDRESS] PRIMARY KEY CLUSTERED
-  (
-  [Id] ASC
-  ) WITH (IGNORE_DUP_KEY = OFF)
+-- Table: MenuItem
+CREATE TABLE MenuItem (
+    Id int  NOT NULL IDENTITY,
+    Category int  NOT NULL,
+    Name varchar(30)  NOT NULL,
+    Description varchar(100)  NOT NULL,
+    Price decimal  NOT NULL,
+    CONSTRAINT MenuItem_pk PRIMARY KEY  (Id)
+);
 
-)
-GO
-CREATE TABLE [RestaurantMenuCategory] (
-	Id int NOT NULL,
-	Name varchar(30) NOT NULL,
-	Restaurant int NOT NULL,
-  CONSTRAINT [PK_RESTAURANTMENUCATEGORY] PRIMARY KEY CLUSTERED
-  (
-  [Id] ASC
-  ) WITH (IGNORE_DUP_KEY = OFF)
+-- Table: Rating
+CREATE TABLE Rating (
+    Id int  NOT NULL IDENTITY,
+    Rank int  NOT NULL,
+    RestaurantTable int  NOT NULL,
+    "User" int  NOT NULL,
+    CONSTRAINT Rating_pk PRIMARY KEY  (Id)
+);
 
-)
-GO
-CREATE TABLE [MenuItem] (
-	Id int NOT NULL,
-	Category int NOT NULL,
-	Name varchar(30) NOT NULL,
-	Description varchar(100) NOT NULL,
-	Price decimal NOT NULL,
-  CONSTRAINT [PK_MENUITEM] PRIMARY KEY CLUSTERED
-  (
-  [Id] ASC
-  ) WITH (IGNORE_DUP_KEY = OFF)
+-- Table: Reservation
+CREATE TABLE Reservation (
+    Id int  NOT NULL IDENTITY,
+    "User" int  NOT NULL,
+    RestaurantTable int  NOT NULL,
+    CONSTRAINT Reservation_pk PRIMARY KEY  (Id)
+);
 
-)
-GO
-CREATE TABLE [RestaurantTable] (
-	Id int NOT NULL,
-	Number int NOT NULL,
-	Restaurant int NOT NULL,
-  CONSTRAINT [PK_RESTAURANTTABLE] PRIMARY KEY CLUSTERED
-  (
-  [Id] ASC
-  ) WITH (IGNORE_DUP_KEY = OFF)
+-- Table: Restaurant
+CREATE TABLE Restaurant (
+    Id int  NOT NULL IDENTITY,
+    Manager int  NOT NULL,
+    Name varchar(50)  NOT NULL,
+    Description varchar(300)  NOT NULL,
+    Image image  NOT NULL,
+    Phone varchar(50)  NOT NULL,
+    Email varchar(100)  NOT NULL,
+    Webste varchar(50)  NOT NULL,
+    Address int  NOT NULL,
+    CONSTRAINT Restaurant_pk PRIMARY KEY  (Id)
+);
 
-)
-GO
-CREATE TABLE [Reservation] (
-	Id int NOT NULL,
-	StartDate datetime NOT NULL,
-	EndDate datetime NOT NULL,
-	[User] int NOT NULL,
-	RestaurantTable int NOT NULL,
-  CONSTRAINT [PK_RESERVATION] PRIMARY KEY CLUSTERED
-  (
-  [Id] ASC
-  ) WITH (IGNORE_DUP_KEY = OFF)
+-- Table: RestaurantMenuCategory
+CREATE TABLE RestaurantMenuCategory (
+    Id int  NOT NULL IDENTITY,
+    Name varchar(30)  NOT NULL,
+    Restaurant int  NOT NULL,
+    CONSTRAINT RestaurantMenuCategory_pk PRIMARY KEY  (Id)
+);
 
-)
-GO
-CREATE TABLE [Comment] (
-	Id int NOT NULL,
-	Description varchar(300) NOT NULL,
-	Restaurant int NOT NULL,
-	[User] int NOT NULL,
-  CONSTRAINT [PK_COMMENT] PRIMARY KEY CLUSTERED
-  (
-  [Id] ASC
-  ) WITH (IGNORE_DUP_KEY = OFF)
+-- Table: RestaurantTable
+CREATE TABLE RestaurantTable (
+    Id int  NOT NULL IDENTITY,
+    Number int  NOT NULL,
+    Restaurant int  NOT NULL,
+    CONSTRAINT RestaurantTable_pk PRIMARY KEY  (Id)
+);
 
-)
-GO
-CREATE TABLE [Rating] (
-	Id int NOT NULL,
-	Rank int NOT NULL,
-	Restaurant int NOT NULL,
-	[User] int NOT NULL,
-  CONSTRAINT [PK_RATING] PRIMARY KEY CLUSTERED
-  (
-  [Id] ASC
-  ) WITH (IGNORE_DUP_KEY = OFF)
+-- Table: Role
+CREATE TABLE Role (
+    Id int  NOT NULL IDENTITY,
+    Name varchar(20)  NOT NULL,
+    CONSTRAINT Role_pk PRIMARY KEY  (Id)
+);
 
-)
-GO
-ALTER TABLE [Restaurant] WITH CHECK ADD CONSTRAINT [Restaurant_fk0] FOREIGN KEY ([Manager]) REFERENCES [User]([Id])
-ON UPDATE CASCADE
-GO
-ALTER TABLE [Restaurant] CHECK CONSTRAINT [Restaurant_fk0]
-GO
-ALTER TABLE [Restaurant] WITH CHECK ADD CONSTRAINT [Restaurant_fk1] FOREIGN KEY ([Address]) REFERENCES [Address]([Id])
-ON UPDATE CASCADE
-GO
-ALTER TABLE [Restaurant] CHECK CONSTRAINT [Restaurant_fk1]
-GO
+-- Table: User
+CREATE TABLE "User" (
+    Id int  NOT NULL IDENTITY,
+    Name varchar(50)  NOT NULL,
+    LastName varchar(50)  NOT NULL,
+    Address varchar(100)  NOT NULL,
+    Email varchar(100)  NOT NULL,
+    Password varchar(50)  NOT NULL,
+    Role int  NOT NULL,
+    CONSTRAINT User_pk PRIMARY KEY  (Id)
+);
 
-ALTER TABLE [User] WITH CHECK ADD CONSTRAINT [User_fk0] FOREIGN KEY ([Role]) REFERENCES [Role]([Id])
-ON UPDATE CASCADE
-GO
-ALTER TABLE [User] CHECK CONSTRAINT [User_fk0]
-GO
+-- foreign keys
+-- Reference: Address_Location (table: Address)
+ALTER TABLE Address ADD CONSTRAINT Address_Location
+    FOREIGN KEY (Location)
+    REFERENCES Location (Id);
 
+-- Reference: Comment_Restaurant (table: Comment)
+ALTER TABLE Comment ADD CONSTRAINT Comment_Restaurant
+    FOREIGN KEY (Restaurant)
+    REFERENCES Restaurant (Id);
 
+-- Reference: Comment_User (table: Comment)
+ALTER TABLE Comment ADD CONSTRAINT Comment_User
+    FOREIGN KEY ("User")
+    REFERENCES "User" (Id);
 
-ALTER TABLE [Address] WITH CHECK ADD CONSTRAINT [Address_fk0] FOREIGN KEY ([Location]) REFERENCES [Location]([Id])
-ON UPDATE CASCADE
-GO
-ALTER TABLE [Address] CHECK CONSTRAINT [Address_fk0]
-GO
+-- Reference: MenuItem_RestaurantMenuCategory (table: MenuItem)
+ALTER TABLE MenuItem ADD CONSTRAINT MenuItem_RestaurantMenuCategory
+    FOREIGN KEY (Category)
+    REFERENCES RestaurantMenuCategory (Id);
 
-ALTER TABLE [RestaurantMenuCategory] WITH CHECK ADD CONSTRAINT [RestaurantMenuCategory_fk0] FOREIGN KEY ([Restaurant]) REFERENCES [Restaurant]([Id])
-ON UPDATE CASCADE
-GO
-ALTER TABLE [RestaurantMenuCategory] CHECK CONSTRAINT [RestaurantMenuCategory_fk0]
-GO
+-- Reference: Rating_RestaurantTable (table: Rating)
+ALTER TABLE Rating ADD CONSTRAINT Rating_RestaurantTable
+    FOREIGN KEY (RestaurantTable)
+    REFERENCES RestaurantTable (Id);
 
-ALTER TABLE [MenuItem] WITH CHECK ADD CONSTRAINT [MenuItem_fk0] FOREIGN KEY ([Category]) REFERENCES [RestaurantMenuCategory]([Id])
-ON UPDATE CASCADE
-GO
-ALTER TABLE [MenuItem] CHECK CONSTRAINT [MenuItem_fk0]
-GO
+-- Reference: Rating_User (table: Rating)
+ALTER TABLE Rating ADD CONSTRAINT Rating_User
+    FOREIGN KEY ("User")
+    REFERENCES "User" (Id);
 
-ALTER TABLE [RestaurantTable] WITH CHECK ADD CONSTRAINT [RestaurantTable_fk0] FOREIGN KEY ([Restaurant]) REFERENCES [Restaurant]([Id])
-ON UPDATE CASCADE
-GO
-ALTER TABLE [RestaurantTable] CHECK CONSTRAINT [RestaurantTable_fk0]
-GO
+-- Reference: Reservation_RestaurantTable (table: Reservation)
+ALTER TABLE Reservation ADD CONSTRAINT Reservation_RestaurantTable
+    FOREIGN KEY (RestaurantTable)
+    REFERENCES RestaurantTable (Id);
 
-ALTER TABLE [Reservation] WITH CHECK ADD CONSTRAINT [Reservation_fk0] FOREIGN KEY ([User]) REFERENCES [User]([Id])
-ON UPDATE CASCADE
-GO
-ALTER TABLE [Reservation] CHECK CONSTRAINT [Reservation_fk0]
-GO
-ALTER TABLE [Reservation] WITH CHECK ADD CONSTRAINT [Reservation_fk1] FOREIGN KEY ([RestaurantTable]) REFERENCES [RestaurantTable]([Id])
+-- Reference: Reservation_User (table: Reservation)
+ALTER TABLE Reservation ADD CONSTRAINT Reservation_User
+    FOREIGN KEY ("User")
+    REFERENCES "User" (Id);
 
-GO
-ALTER TABLE [Reservation] CHECK CONSTRAINT [Reservation_fk1]
-GO
+-- Reference: RestaurantMenuCategory_Restaurant (table: RestaurantMenuCategory)
+ALTER TABLE RestaurantMenuCategory ADD CONSTRAINT RestaurantMenuCategory_Restaurant
+    FOREIGN KEY (Restaurant)
+    REFERENCES Restaurant (Id);
 
-ALTER TABLE [Comment] WITH CHECK ADD CONSTRAINT [Comment_fk0] FOREIGN KEY ([Restaurant]) REFERENCES [Restaurant]([Id])
-ON UPDATE CASCADE
-GO
-ALTER TABLE [Comment] CHECK CONSTRAINT [Comment_fk0]
-GO
-ALTER TABLE [Comment] WITH CHECK ADD CONSTRAINT [Comment_fk1] FOREIGN KEY ([User]) REFERENCES [User]([Id])
+-- Reference: RestaurantTable_Restaurant (table: RestaurantTable)
+ALTER TABLE RestaurantTable ADD CONSTRAINT RestaurantTable_Restaurant
+    FOREIGN KEY (Restaurant)
+    REFERENCES Restaurant (Id);
 
-GO
-ALTER TABLE [Comment] CHECK CONSTRAINT [Comment_fk1]
-GO
+-- Reference: Restaurant_Address (table: Restaurant)
+ALTER TABLE Restaurant ADD CONSTRAINT Restaurant_Address
+    FOREIGN KEY (Address)
+    REFERENCES Address (Id);
 
-ALTER TABLE [Rating] WITH CHECK ADD CONSTRAINT [Rating_fk0] FOREIGN KEY ([Restaurant]) REFERENCES [Restaurant]([Id])
-ON UPDATE CASCADE
-GO
-ALTER TABLE [Rating] CHECK CONSTRAINT [Rating_fk0]
-GO
-ALTER TABLE [Rating] WITH CHECK ADD CONSTRAINT [Rating_fk1] FOREIGN KEY ([User]) REFERENCES [User]([Id])
+-- Reference: Restaurant_User (table: Restaurant)
+ALTER TABLE Restaurant ADD CONSTRAINT Restaurant_User
+    FOREIGN KEY (Manager)
+    REFERENCES "User" (Id);
 
-GO
-ALTER TABLE [Rating] CHECK CONSTRAINT [Rating_fk1]
-GO
+-- Reference: User_Role (table: User)
+ALTER TABLE "User" ADD CONSTRAINT User_Role
+    FOREIGN KEY (Role)
+    REFERENCES Role (Id);
+
+-- End of file.
 
