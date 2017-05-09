13b85f997d3e2fceac0d6b5ee4e7c702e6cefbba
anica <anica.sutic@gmail.com>
2017-05-09 21:44:49 +0200

    Added date and time picker for reservation.

--- TakeYourSeat/app/src/main/java/takeyourseat/activities/ReservationActivity.java
+++ TakeYourSeat/app/src/main/java/takeyourseat/activities/ReservationActivity.java
@@ -15,50 +15,21 @@ import com.example.anica.takeyourseat.R;
 
 import java.text.DateFormat;
 import java.util.Calendar;
-import java.util.GregorianCalendar;
 
-public class ReservationActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
-
-    private Button dateButton;
-    private EditText dateRes;
+public class ReservationActivity extends AppCompatActivity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reservation);
-
-        dateButton = (Button) findViewById(R.id.dateButton);
-        dateRes = (EditText) findViewById(R.id.dateRes);
     }
 
 
-    public void datePicker(View view) {
-        DatePickerFragment fragment = new DatePickerFragment();
-        fragment.show(getSupportFragmentManager(), "date");
 
-    }
 
-    private void setDate(final Calendar calendar) {
-        final DateFormat df = DateFormat.getDateInstance(DateFormat.MEDIUM);
-        dateRes.setText(df.format(calendar.getTime()));
-    }
-    @Override
-    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-        Calendar cal = new GregorianCalendar(year,month,dayOfMonth);
-        setDate(cal);
-    }
 
-    public static class DatePickerFragment extends DialogFragment {
-
-        @Override
-        public Dialog onCreateDialog(Bundle savedInstanceState) {
-            final Calendar c = Calendar.getInstance();
-            int year = c.get(Calendar.YEAR);
-            int month = c.get(Calendar.MONTH);
-            int day = c.get(Calendar.DAY_OF_MONTH);
-
-            return new DatePickerDialog(getActivity(),(DatePickerDialog.OnDateSetListener)getActivity(),year,month,day);
-
-        }
-    }
 }
+
+
+
+
--- TakeYourSeat/app/src/main/java/takeyourseat/fragments/TableFragment.java
+++ TakeYourSeat/app/src/main/java/takeyourseat/fragments/TableFragment.java
@@ -1,6 +1,8 @@
 package takeyourseat.fragments;
 
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AlertDialog;
@@ -8,9 +10,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TimePicker;
 
 import com.example.anica.takeyourseat.R;
+
+import java.text.DateFormat;
+import java.util.Calendar;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -22,6 +29,10 @@ public class TableFragment extends Fragment {
     private EditText lastNameRes;
     private EditText date;
     private Button reserve;
+    private EditText dateRes;
+    private EditText timeRes;
+    Calendar calendar = Calendar.getInstance();
+    DateFormat format = DateFormat.getDateTimeInstance();
 
 
     public TableFragment() {
@@ -43,14 +54,53 @@ public class TableFragment extends Fragment {
                 nameRes = (EditText) mView.findViewById(R.id.nameRes);
                 lastNameRes = (EditText) mView.findViewById(R.id.lastNameRes);
                 reserve = (Button) mView.findViewById(R.id.reserve);
+                dateRes = (EditText) mView.findViewById(R.id.dateRes);
+                timeRes = (EditText) mView.findViewById(R.id.timeRes);
 
                 builder.setView(mView);
                 AlertDialog dialog = builder.create();
                 dialog.show();
+
+                dateRes.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        updateDate();
+                    }
+                });
+
+                timeRes.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        updateTime();
+                    }
+                });
             }
         });
         // Inflate the layout for this fragment
         return v;
     }
+
+    private void updateDate() {
+        new DatePickerDialog(getActivity(),d,calendar.get(Calendar.YEAR),calendar.get(Calendar.MONTH),calendar.get(Calendar.DAY_OF_MONTH)).show();
+    }
+
+    private void updateTime() {
+        new TimePickerDialog(getActivity(),t,calendar.get(Calendar.HOUR),calendar.get(Calendar.MINUTE),true).show();
+    }
+
+    DatePickerDialog.OnDateSetListener d = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                dateRes.setText(dayOfMonth + "-" + (month + 1) + "-" + year);
+        }
+    };
+
+    TimePickerDialog.OnTimeSetListener t = new TimePickerDialog.OnTimeSetListener() {
+        @Override
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                timeRes.setText(hourOfDay + ":" + minute);
+        }
+    };
+
 
 }
