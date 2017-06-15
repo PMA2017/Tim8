61a09aa39ce35139bedf2af32a714e57b3993af9
Nenad <nenad.rad933@gmail.com>
2017-06-16 01:04:31 +0200

    Refactoring & added method in API interface for fetching reserved tables.

---
+++ TakeYourSeat/app/src/main/java/takeyourseat/model/ReservationTable.java
@@ -0,0 +1,45 @@
+package takeyourseat.beans;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by Nenad on 6/16/2017.
+ */
+
+public class ReservationTable {
+
+    @SerializedName("Id")
+    @Expose
+    private Integer id;
+    @SerializedName("Number")
+    @Expose
+    private Integer number;
+    @SerializedName("StartDate")
+    @Expose
+    private String startDate;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+}
