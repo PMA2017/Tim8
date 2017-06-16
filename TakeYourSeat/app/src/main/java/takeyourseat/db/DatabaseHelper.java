4ddfe5899b0738a19a31af2163bd31462aa566ca
anica <anica.sutic@gmail.com>
2017-06-16 16:26:56 +0200

    Added crud operation for ORMLite database, created UserRepoCLass.

--- TakeYourSeat/app/src/main/java/takeyourseat/db/DatabaseHelper.java
+++ TakeYourSeat/app/src/main/java/takeyourseat/db/DatabaseHelper.java
@@ -1,9 +1,19 @@
 package takeyourseat.db;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
 
 import com.j256.ormlite.android.apptools.OrmLiteSqliteOpenHelper;
+import com.j256.ormlite.dao.Dao;
 import com.j256.ormlite.support.ConnectionSource;
+import com.j256.ormlite.table.TableUtils;
+
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import takeyourseat.model.User;
 
 
 /**
@@ -17,6 +27,7 @@ public class DatabaseHelper extends OrmLiteSqliteOpenHelper {
 
     // any time you make changes to your database objects, you may have to increase the database version
     private static final int DATABASE_VERSION = 1;
+    private Dao<User, Integer> userDao = null;
 
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -24,11 +35,43 @@ public class DatabaseHelper extends OrmLiteSqliteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase database, ConnectionSource connectionSource) {
+       /* try {
+            TableUtils.createTable(connectionSource, User.class);
+        } catch (SQLException e) {
+            Log.e(DatabaseHelper.class.getName(), "Can't create database", e);
+            throw new RuntimeException(e);
+        }*/
+
 
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, ConnectionSource connectionSource, int oldVersion, int newVersion) {
+       /* try {
+            List<String> allSql = new ArrayList<String>();
+            switch(oldVersion)
+            {
+                case 1:
+                    //allSql.add("altere AdData add column `new_col` VARCHAR");
+                    //allSql.add("altere AdData add column `new_col2` VARCHAR");
+            }
+            for (String sql : allSql) {
+                db.execSQL(sql);
+            }
+        } catch (SQLException e) {
+            Log.e(DatabaseHelper.class.getName(), "exception during onUpgrade", e);
+            throw new RuntimeException(e);
+        }*/
+    }
 
+    public Dao<User, Integer> getUserDao() {
+        if (null == userDao) {
+            try {
+                userDao = getDao(User.class);
+            }catch (java.sql.SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return userDao;
     }
 }
