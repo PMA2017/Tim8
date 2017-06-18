0273f195d725d2276f206ba747513e2c54a67ca1
anica <anica.sutic@gmail.com>
2017-06-18 12:09:15 +0200

    Added url to uset image path, fixed edit profile and change password.

--- TakeYourSeat/app/src/main/java/takeyourseat/activities/EditPasswordActivity.java
+++ TakeYourSeat/app/src/main/java/takeyourseat/activities/EditPasswordActivity.java
@@ -1,18 +1,25 @@
 package takeyourseat.activities;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.InputType;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.anica.takeyourseat.R;
 import com.j256.ormlite.android.apptools.OpenHelperManager;
 
 import java.sql.SQLException;
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -29,6 +36,8 @@ public class EditPasswordActivity extends AppCompatActivity {
     private ApiService apiService;
     private DatabaseHelper databaseHelper;
     private User currentUser;
+    private TextView showPass,showRePass;
+    private int userId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +46,85 @@ public class EditPasswordActivity extends AppCompatActivity {
 
         newPassword = (EditText)findViewById(R.id.changeNewPassEdit);
         repeatPassword = (EditText)findViewById(R.id.repeatNewPassEdit);
+
+        showPass = (TextView) findViewById(R.id.showPassProfile);
+        showRePass = (TextView) findViewById(R.id.showRePassProfile);
+
+        showPass.setVisibility(View.GONE);
+        showRePass.setVisibility(View.GONE);
+        newPassword.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (newPassword.getText().length() > 0) {
+                    showPass.setVisibility(View.VISIBLE);
+                } else {
+                    showPass.setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        repeatPassword.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (repeatPassword.getText().length() > 0) {
+                    showRePass.setVisibility(View.VISIBLE);
+                } else {
+                    showRePass.setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        showPass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (showPass.getText() == "SHOW PASSWORD") {
+                    showPass.setText("HIDE PASSWORD");
+                    newPassword.setInputType(InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                    newPassword.setSelection(newPassword.length());
+
+                } else {
+                    showPass.setText("SHOW PASSWORD");
+                    newPassword.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                    newPassword.setSelection(newPassword.length());
+                }
+            }
+        });
+
+        showRePass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (showRePass.getText() == "SHOW PASSWORD") {
+                    showRePass.setText("HIDE PASSWORD");
+                    repeatPassword.setInputType(InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                    repeatPassword.setSelection(newPassword.length());
+
+                } else {
+                    showRePass.setText("SHOW PASSWORD");
+                    repeatPassword.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                    repeatPassword.setSelection(newPassword.length());
+                }
+            }
+        });
 
         editBtn = (Button)findViewById(R.id.changePassBtn);
 
@@ -54,13 +142,17 @@ public class EditPasswordActivity extends AppCompatActivity {
 
         apiService = ApiUtils.getApiService();
 
-        currentUser = getCurrentUser();
+
+        SharedPreferences sharedPreferences = getSharedPreferences("userDetails", Context.MODE_PRIVATE);
+        String token = sharedPreferences.getString("token","");
+        currentUser = getCurrentUser(token);
     }
 
-    private User getCurrentUser() {
-        int userId = getIntent().getExtras().getInt("userId");
+    private User getCurrentUser(String token) {
+        userId = getIntent().getExtras().getInt("userId");
         try {
-            currentUser = getDatabaseHelper().getUserDao().queryForEq("Id", userId).get(0);
+            List<User> userr = getDatabaseHelper().getUserDao().queryBuilder().where().eq("token",token).query();
+            currentUser = userr.get(0);
             return currentUser;
         } catch (SQLException e) {
             Log.e("EditPasswordActivity", e.getMessage());
@@ -71,7 +163,7 @@ public class EditPasswordActivity extends AppCompatActivity {
     private void editPassword() {
         if(validate()) {
             currentUser.setPassword(newPassword.getText().toString());
-            apiService.updateUser("Id", currentUser.getId().toString(), currentUser).enqueue(new Callback<Boolean>() {
+            apiService.updateUser("Id", Integer.toString(userId), currentUser).enqueue(new Callback<Boolean>() {
                 @Override
                 public void onResponse(Call<Boolean> call, Response<Boolean> response) {
                     if(!response.isSuccessful()) {
@@ -101,6 +193,11 @@ public class EditPasswordActivity extends AppCompatActivity {
     private boolean validate() {
         if(newPassword.getText().toString().isEmpty()){
             newPassword.setError("Please enter a password.");
+            return false;
+        }
+
+        if(newPassword.getText().toString().length() < 6){
+            newPassword.setError("Password must be at least 6 characters long!");
             return false;
         }
 
