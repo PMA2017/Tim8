cfc2ef9d4609321fbe3e29818c3286aeae138e70
Nenad <nenad.rad933@gmail.com>
2017-09-10 14:38:27 +0200

    Added new model class for completing reservation.

---
+++ TakeYourSeat/app/src/main/java/takeyourseat/model/CompleteReservation.java
@@ -0,0 +1,50 @@
+package takeyourseat.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+/**
+ * Created by Nenad on 9/10/2017.
+ */
+
+public class CompleteReservation {
+
+    @SerializedName("Reservation")
+    @Expose
+    private Reservation reservation;
+
+    @SerializedName("TableIds")
+    @Expose
+    private List<String> tableIds;
+
+    @SerializedName("FriendIds")
+    @Expose
+    private List<String> friendIds;
+
+
+    public List<String> getTableIds() {
+        return tableIds;
+    }
+
+    public void setTableIds(List<String> tableIds) {
+        this.tableIds = tableIds;
+    }
+
+    public List<String> getFriendIds() {
+        return friendIds;
+    }
+
+    public void setFriendIds(List<String> friendIds) {
+        this.friendIds = friendIds;
+    }
+
+    public Reservation getReservation() {
+        return reservation;
+    }
+
+    public void setReservation(Reservation reservation) {
+        this.reservation = reservation;
+    }
+}
